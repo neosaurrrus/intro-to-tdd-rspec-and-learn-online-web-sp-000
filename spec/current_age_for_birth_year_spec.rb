a6175477c82321ebc672fd80b2df4971abc74db4
Lukie Kang <lukiek+f@gmail.com>
2019-05-09 22:26:59 +0000

    Automatically backed up by Learn

--- spec/current_age_for_birth_year_spec.rb
+++ spec/current_age_for_birth_year_spec.rb
@@ -5,6 +5,6 @@ describe "current_age_for_birth_year method" do
     answer = Time.now.year - birth_year
     age_of_person = current_age_for_birth_year(1984)
 
-    expect(age_of_person).to eq(19)
+    expect(age_of_person).to eq(answer)
   end
 end
